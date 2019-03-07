265cebdbb87f256f50628bb4f4ad2d475f8a6e6d
george <george@gomaji.com>
2019-03-07 16:02:50 +0800

    1.初步完成MainPage

---
+++ lib/assets.dart
@@ -0,0 +1,10 @@
+class ImageAssets {
+  static const String transparentImage = 'assets/images/1x1_transparent.png';
+  static const String backgroundImage = 'assets/images/background_image.jpg';
+  static const String poweredByTMDBLogo = 'assets/images/powered_by_tmdb.png';
+  static const String app_logo_icon = 'assets/images/logo.png';
+}
+
+class OtherAssets {
+  static const String preloadedTheaters = 'assets/preloaded_data/theaters.xml';
+}
---
+++ lib/main_page.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_movie_app/assets.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+const tabTitles = ["首頁", "搜尋", "戲院"];
+
+class _MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  int _selectedTab = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final backgroundImage = Image.asset(
+      ImageAssets.backgroundImage,
+      fit: BoxFit.cover,
+    );
+
+    final content = Scaffold(
+      appBar: AppBar(
+        title: new Text(tabTitles[_selectedTab],
+            style: TextStyle(color: Colors.white)),
+        iconTheme: new IconThemeData(color: Colors.white),
+        backgroundColor: Color(0xFF152451),
+        leading: Image.asset(ImageAssets.app_logo_icon), //app_logo_icon
+      ),
+      body: Stack(
+        children: [
+          _buildBodyContent(),
+          _BottomTabs(
+            selectedTab: _selectedTab,
+            onTap: _tabSelected,
+          ),
+        ],
+      ),
+    );
+
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        backgroundImage,
+        content,
+      ],
+    );
+  }
+
+  void _tabSelected(int newIndex) {
+    setState(() {
+      _selectedTab = newIndex;
+      _tabController.index = newIndex;
+    });
+  }
+
+  Widget _buildBodyContent() {
+    return Positioned.fill(
+      child: TabBarView(
+        controller: _tabController,
+        children: [
+          ///todo 實作三個content widget
+          Center(child: Text("page1", style: TextStyle(color: Colors.white))),
+          Center(child: Text("page2", style: TextStyle(color: Colors.white))),
+          Center(child: Text("page3", style: TextStyle(color: Colors.white)))
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomTabs extends StatelessWidget {
+  _BottomTabs({
+    @required this.selectedTab,
+    @required this.onTap,
+  });
+
+  final int selectedTab;
+  final ValueChanged<int> onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+        alignment: Alignment.bottomCenter,
+        child: CupertinoTabBar(
+          backgroundColor: Colors.black54,
+          inactiveColor: Colors.white54,
+          activeColor: Colors.white,
+          iconSize: 24.0,
+          currentIndex: selectedTab,
+          onTap: onTap,
+          items: [
+            BottomNavigationBarItem(
+              title: Text(tabTitles[0]),
+              icon: const Icon(Icons.local_movies),
+              backgroundColor: Theme.of(context).primaryColor,
+            ),
+            BottomNavigationBarItem(
+              title: Text(tabTitles[1]),
+              icon: const Icon(Icons.search),
+              backgroundColor: Theme.of(context).primaryColor,
+            ),
+            BottomNavigationBarItem(
+              title: Text(tabTitles[2]),
+              icon: const Icon(Icons.store_mall_directory),
+              backgroundColor: Theme.of(context).primaryColor,
+            ),
+          ],
+        ));
+  }
+}
